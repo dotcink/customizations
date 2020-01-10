b0280a2d71bc5460810ab08facff8d4bd248a6e3
Dot Cink <Dot.Cink+github@gmail.com>
2020-01-11 00:41:01 +0800

    add bash_profile

---
+++ bash_profile.sh
@@ -0,0 +1,29 @@
+
+########  ########
+# shell
+########  ########
+
+# ignoreboth = ignorespace + ignoredups
+export HISTCONTROL=ignoreboth:erasedups
+# as more lines as possible
+export HISTFILESIZE=999999
+export HISTSIZE=999999
+# append, instead of overwriting
+shopt -s histappend
+
+# colorized
+alias ls='ls -G'
+
+########  ########
+# vim
+########  ########
+
+export EDITOR=vim
+# skip the use of .viminfo file(no record of vim operations)
+alias vimN='vim -i NONE'
+
+########  ########
+# others
+########  ########
+
+alias px='ps ax | grep '
