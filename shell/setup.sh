bb0b8ef5f8a4cfab6278a348e49f8d08fa2db9f6
Dot Cink <Dot.Cink+github@gmail.com>
2020-01-17 11:46:04 +0800

    [shell] source rc files directly instead of cloning & composite shell rc's to 'rs'

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,14 +1,29 @@
+#!/bin/sh
 # vim: ft=sh
 
 BASE_DIR=$(dirname $0)
-SHRC_PROXY=$HOME/.shrc_proxy
+SHRC_PATH=customizations/shell/rc
+SHRC=$HOME/$SHRC_PATH
 
-# auto clone shrc_proxy if not exist
-if [[ -f $SHRC_PROXY ]]; then
-  echo "$SHRC_PROXY already exist"
-else
-  ln -v $HOME/customizations/shell/rc_proxy $SHRC_PROXY
-fi
-. $SHRC_PROXY
+source $SHRC
+
+# Simplify the process by `source` the repo's rc files directly in base rc's,
+# instead of cloning a file to $HOME and `source` the cloned
+function add_source_shrc() {
+  base_rc=$1
+  # not create if non exist, because the file mostly is not created by user
+  # add `source` command if not added once
+  if [ -w "$base_rc" ] && [ "$(grep -c "^source .*$SHRC_PATH$" "$base_rc")" = 0 ]; then
+    echo "\nsource $SHRC" >> $base_rc
+  else
+    if [[ ! -w "$base_rc" ]]; then
+      echo "Base rc '$base_rc' to add source is NOT existent or writable."
+    fi
+  fi
+}
+
+add_source_shrc ~/.zshrc
 
 $BASE_DIR/zsh/setup.sh
+
+add_source_shrc ~/.zshrc
