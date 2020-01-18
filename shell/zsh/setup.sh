9951a7439a7b0b0df7bad46808efd246ba9c17bd
Dot Cink <Dot.Cink+github@gmail.com>
2020-01-18 23:47:59 +0800

    [zsh] change theme to powerlevel9k

--- shell/zsh/setup.sh
+++ shell/zsh/setup.sh
@@ -16,5 +16,11 @@ powerline_fonts_dir=$BASE_DIR/../../theme/powerline-fonts
 git submodule update --init --progress $powerline_fonts_dir
 $powerline_fonts_dir/install.sh
 
+# Install Powerlevel9k theme
+powerlevel9k_link_to=~/.oh-my-zsh/custom/themes/powerlevel9k
+powerlevel9k_theme_dir=$BASE_DIR/theme/powerlevel9k
+git submodule update --init --progress $powerlevel9k_theme_dir
+[ ! -f $powerlevel9k_link_to ] && ln -s ~/customizations/shell/zsh/theme/powerlevel9k $powerlevel9k_link_to
+
 # set DEFAULT_USER to hide "user@host" prompt prefix
 include_rc_to_local ~/.zshrc "export DEFAULT_USER=$USER" '^export DEFAULT_USER=' ''
